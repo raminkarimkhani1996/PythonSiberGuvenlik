a921f1e53a0d096ff18d577cd7d0fd7c272a984d
raminkarimkhani1996 <raminkarimkhani1996@gmail.com>
2018-04-03 19:36:13 +0300

    NTLM Kod Çözücü
    Programın amacı NTLM olarak hashlenmiş ifadeleri çözümlemektir.

---
+++ NTLMdecoder.py
@@ -0,0 +1,20 @@
+#Programın amacı parola listesine bağlı olarak NTLM kodlanmış ifadeleri çözmektir.
+import hashlib
+try:
+	cozulecekNTLM=input("Kodu çözülecek NTLM Hash:")
+	parolaDosyasi=input('Parola Dosyasının Adı:')
+	parolalar=[]
+	file=open(parolaDosyasi,'r')
+	for parola in file:
+		NTLM=hashlib.new('md4',parola.rstrip('\n').encode('utf-16le')).hexdigest()
+		if(NTLM==cozulecekNTLM):
+			print('PAROLA:'+parola)
+			exit()
+		else:
+			continue
+except FileNotFoundError:
+	print("Lütfen dosya adını doğru giriniz.Eğer dosya farklı bir konumdaysa, konum yolu ile beraber giriniz.")
+except UnicodeDecodeError:
+	print("Aranan parola bulunamadı")
+except IsADirectoryError:
+	print("Lütfen klasör adını doğru giriniz")
